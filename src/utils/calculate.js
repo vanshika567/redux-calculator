2512cf5a2147b2a87840044d899115eff8a69086
vanshika567 <vanshika.bhatt@eisenvault.com>
2021-04-19 11:40:37 +0530

    screen issue removed

--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -1,7 +1,5 @@
 const Calculate=(expression) => {
-    var newfn=function(){
-      return `${matched[0]}`
-    }
+
     const matched = (new RegExp('([\\d]+\\.?[\\d]*)?([-+/*][\\d]+\\.?[\\d]*)*')).exec(expression)
   
     if (!matched) {
@@ -13,6 +11,6 @@ const Calculate=(expression) => {
         throw new Error('Cannot start the expression with invalid operators')
       }
     }
-    return newfn()
+    return new Function(`return ${matched[0]}`)()
   }
   export default Calculate;
